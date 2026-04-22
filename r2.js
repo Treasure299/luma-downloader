a0cbec269720ba75540bb8ec342debc5e7f40a13
Treasure299 <dancingkeys.me@gmail.com>
2026-04-22 09:01:26 +0200

    Update r2.js

--- r2.js
+++ r2.js
@@ -9,22 +9,26 @@ signatureVersion: 'v4',
 });
 
 async function uploadToR2(filePath) {
+if (!process.env.R2_BUCKET_NAME) {
+throw new Error('R2_BUCKET_NAME missing');
+}
+
 const fileContent = fs.readFileSync(filePath);
 
-const fileName = 'videos/' + Date.now() + '.mp4';
+const key = 'videos/' + Date.now() + '.mp4';
 
 const params = {
 Bucket: process.env.R2_BUCKET_NAME,
-Key: fileName,
+Key: key,
 Body: fileContent,
 ContentType: 'video/mp4',
 };
 
 await s3.putObject(params).promise();
 
-return process.env.R2_PUBLIC_URL + '/' + fileName;
+return process.env.R2_PUBLIC_URL + '/' + key;
 }
 
 module.exports = {
-uploadToR2,
+uploadToR2: uploadToR2,
 };
