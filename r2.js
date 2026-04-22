4e612bb0c2e1caa07f63195a16711bdfddccdce3
Treasure299 <dancingkeys.me@gmail.com>
2026-04-22 08:56:41 +0200

    Update r2.js

--- r2.js
+++ r2.js
@@ -2,25 +2,29 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
-endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+endpoint: 'https://' + process.env.R2_ACCOUNT_ID + '.r2.cloudflarestorage.com',
 accessKeyId: process.env.R2_ACCESS_KEY_ID,
 secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
 signatureVersion: 'v4',
 });
 
-const uploadToR2 = async (filePath) => {
+async function uploadToR2(filePath) {
 const fileContent = fs.readFileSync(filePath);
+
+const fileName = 'videos/' + Date.now() + '.mp4';
 
 const params = {
 Bucket: process.env.R2_BUCKET_NAME,
-Key: `videos/${Date.now()}.mp4`,
+Key: fileName,
 Body: fileContent,
 ContentType: 'video/mp4',
 };
 
 await s3.putObject(params).promise();
 
-return `${process.env.R2_PUBLIC_URL}/${params.Key}`;
-};
+return process.env.R2_PUBLIC_URL + '/' + fileName;
+}
 
-module.exports = { uploadToR2 };
+module.exports = {
+uploadToR2,
+};
