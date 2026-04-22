acb2050c6079cab04c9123d7116a66fd6aef674d
Treasure299 <dancingkeys.me@gmail.com>
2026-04-22 09:28:05 +0200

    Update download.js

--- download.js
+++ download.js
@@ -1,29 +1,33 @@
 const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs');
 
-const downloadVideo = (url) => {
+function downloadVideo(url) {
   return new Promise((resolve, reject) => {
-    const filePath = path.join(__dirname, 'output.mp4');
 
-    const command = `./yt-dlp -f best -o "${filePath}" "${url}"`;
+    if (!url) {
+      return reject(new Error('No URL provided'));
+    }
 
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.error('YT-DLP ERROR:', err);
+    const command = `./yt-dlp -f best -o output.mp4 "${url}"`;
+
+    exec(command, (error, stdout, stderr) => {
+
+      if (error) {
+        console.error('YT-DLP ERROR:', error);
         console.error('STDERR:', stderr);
-        return reject(new Error(stderr || err.message));
+        return reject(new Error(stderr || error.message));
       }
 
-      console.log('YT-DLP OUTPUT:', stdout);
+      console.log('YT-DLP SUCCESS:', stdout);
 
       resolve({
         success: true,
         message: 'Download completed',
-        videoUrl: 'TEMP_LOCAL_FILE', // we will replace this with R2 next
+        videoUrl: 'TEMP_LOCAL_FILE'
       });
+
     });
+
   });
-};
+}
 
 module.exports = downloadVideo;
